a52c42d1f17dc4380fb11e702fe4b3e317bd38bb
Hoang Manh Khiem <khiemhm04@gmail.com>
2024-08-11 18:51:29 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,11 @@
-import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
 from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA
 from scipy.stats import zscore
 data = pd.read_csv("CC GENERAL.csv").drop(columns="CUST_ID")
-data.fillna(data.mean(), inplace=True)
-data_scaled = data.apply(zscore)
-kmeans = KMeans(n_clusters=4, random_state=0).fit(data_scaled)
-labels = kmeans.labels_
+data_scaled = data.apply(lambda x: zscore(x.fillna(x.mean())))
+labels = KMeans(n_clusters=4, random_state=0).fit_predict(data_scaled)
 data_pca = PCA(n_components=2).fit_transform(data_scaled)
-sns.scatterplot(x=data_pca[:, 0], y=data_pca[:, 1], hue=labels, palette='viridis', s=60, alpha=0.7)
-plt.title('KMeans Clustering Results')
-plt.legend(title='Cluster')
+plt.scatter(data_pca[:, 0], data_pca[:, 1], c=labels, cmap='viridis', s=60, alpha=0.7)
 plt.show()
